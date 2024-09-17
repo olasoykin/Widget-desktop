1085a77401fd321369531415dc87bdae6d08205a
Rastersoft <rastersoft@gmail.com>
2024-09-17 17:43:55 +0000

    Use meson to install the apparmor file

--- local_install.sh
+++ local_install.sh
@@ -3,6 +3,6 @@
 rm -rf ~/.local/share/gnome-shell/extensions/ding@rastersoft.com/*
 rm -rf .build
 mkdir .build
-meson --prefix=$HOME/.local/ --localedir=share/gnome-shell/extensions/ding@rastersoft.com/locale .build
+meson setup --prefix=$HOME/.local/ --localedir=share/gnome-shell/extensions/ding@rastersoft.com/locale .build
 ninja -C .build install
 rm -rf .build
